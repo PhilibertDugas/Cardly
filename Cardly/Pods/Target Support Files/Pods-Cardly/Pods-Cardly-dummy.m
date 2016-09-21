9fe3c6ea4157ae18d934e1e3ec63eafc2e13eb8e
Philibert Dugas <philibert.dugas@gmail.com>
2016-09-20 22:02:16 -0400

    Remove the Pods folder from git

--- Cardly/Pods/Target Support Files/Pods-Cardly/Pods-Cardly-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cardly : NSObject
-@end
-@implementation PodsDummy_Pods_Cardly
-@end
--- Cardly/Pods/Target Support Files/Pods-CardlyTests/Pods-CardlyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CardlyTests : NSObject
-@end
-@implementation PodsDummy_Pods_CardlyTests
-@end
