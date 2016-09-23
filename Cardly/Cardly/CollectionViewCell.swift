e166e50038279391834a5871fabb3a241f2237b9
Guillaume Lalande <guillaume.lalande@polymtl.ca>
2016-09-23 13:41:51 -0400

    Added profile view structure

---
+++ Cardly/Cardly/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Cardly
+//
+//  Created by Guillaume Lalande on 2016-09-23.
+//  Copyright © 2016 QH4L. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
